b5a089ff937f00bd017647098f0ea1f03ea3b4e8
Fusion <leemenhin@gmail.com>
2021-03-20 17:04:41 +0800

    Add files via upload

--- build/ramdisk.sh
+++ build/ramdisk.sh
@@ -1,12 +1,12 @@
-sudo rm -rf build/ramdisk/sbin/dm_verity_signature_checker build/ramdisk/sbin/dm_verity_tz_cmd build/ramdisk/sbin/knox_changer
+rm -rf build/ramdisk/sbin/dm_verity_signature_checker build/ramdisk/sbin/dm_verity_tz_cmd build/ramdisk/sbin/knox_changer
 
-sudo cp build/ramdiskmod/sbin/ build/ramdisk/sbin/
+cp -R build/ramdiskmod/* build/ramdisk/
 
-sudo sed -i '/enforcing/d' build/ramdisk/default.prop
+sed -i '/enforcing/d' build/ramdisk/default.prop
 
-sudo sed -i '/secure/d' build/ramdisk/default.prop
+sed -i '/secure/d' build/ramdisk/default.prop
 
-sudo sed -i '/debuggable/d' build/ramdisk/default.prop
+sed -i '/debuggable/d' build/ramdisk/default.prop
 
-sudo cat build/ramdiskmod/propmod >> build/ramdisk/default.prop
+cat build/ramdiskmod/propmod >> build/ramdisk/default.prop
 
