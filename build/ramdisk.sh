9b6f5152e28b4dbf4d3b6cf08af2611507f7d399
Fusion <leemenhin@gmail.com>
2021-03-20 16:28:05 +0800

    Update ramdisk.sh

--- build/ramdisk.sh
+++ build/ramdisk.sh
@@ -1,12 +1,12 @@
-rm -rf build/ramdisk/sbin/dm_verity_signature_checker build/ramdisk/sbin/dm_verity_tz_cmd build/ramdisk/sbin/knox_changer
+sudo rm -rf build/ramdisk/sbin/dm_verity_signature_checker build/ramdisk/sbin/dm_verity_tz_cmd build/ramdisk/sbin/knox_changer
 
-cp build/ramdiskmod/sbin/ build/ramdisk/sbin/
+sudo cp build/ramdiskmod/sbin/ build/ramdisk/sbin/
 
-sed -i '/enforcing/d' build/ramdisk/default.prop
+sudo sed -i '/enforcing/d' build/ramdisk/default.prop
 
-sed -i '/secure/d' build/ramdisk/default.prop
+sudo sed -i '/secure/d' build/ramdisk/default.prop
 
-sed -i '/debuggable/d' build/ramdisk/default.prop
+sudo sed -i '/debuggable/d' build/ramdisk/default.prop
 
-cat build/ramdiskmod/propmod >> build/ramdisk/default.prop
+sudo cat build/ramdiskmod/propmod >> build/ramdisk/default.prop
 
